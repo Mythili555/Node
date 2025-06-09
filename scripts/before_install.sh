ebbd2067093756d4ddba23a473a734f1ff646afa
Mythili555 <116316394+Mythili555@users.noreply.github.com>
2025-06-09 17:19:09 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # cd /var/www/nodejs
 # sudo pm2 stop api
+#!/bin/bash
+
+# Make sure ownership is right
+sudo chown -R ubuntu:ubuntu /var/www/nodejs
+
+# Ensure scripts are executable
+sudo chmod +x /var/www/nodejs/scripts/*.sh
 
