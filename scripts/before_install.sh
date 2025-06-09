e999434da8f7f14739cb8cc47fe2e03824e8f4e9
Mythili555 <116316394+Mythili555@users.noreply.github.com>
2025-06-09 17:14:17 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 # cd /var/www/nodejs
 # sudo pm2 stop api
-#!/bin/bash
 
-# Move to your app directory
-cd /var/www/nodejs
-
-# Install Node.js dependencies
-npm install
-
-# Install PM2 globally if not already installed
-sudo npm install -g pm2
-
-# Optional: ensure proper permissions (if needed)
-chown -R ubuntu:ubuntu /var/www/nodejs
-chmod -R 755 /var/www/nodejs
